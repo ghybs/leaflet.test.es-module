51da544245688b2a8f9c3fa7c317a4ec96e0aa5b
ghybs <ghybs1@gmail.com>
2018-01-22 13:08:32 +0400

    Chore(build): get build engine as parameter
    so that we can re-use the same build script for Rollup and webpack (possibly Browserify as well later on?)

---
+++ build/build.js
@@ -0,0 +1,49 @@
+/**
+ * Created by boris on 7/17/17.
+ */
+
+// To be run with node.
+var exec = require('child_process').exec;
+
+
+// Retrieve build engine.
+var buildEngine = process.argv[2];
+var availableBuildEngines = ['rollup', 'webpack'];
+
+if (typeof buildEngine !== 'string' || !availableBuildEngines.includes(buildEngine.toLowerCase())) {
+  throw 'Please specify a build engine as 1st argument. Available engines: ' + availableBuildEngines;
+}
+buildEngine = buildEngine.toLowerCase();
+
+// Retrieve source folder.
+// https://nodejs.org/docs/latest/api/process.html#process_process_argv
+var folderName = process.argv[3];
+
+if (!folderName) {
+  throw 'Please specify the folder path to build.';
+}
+
+// Retrieve configuration file name, if specified.
+// https://rollupjs.org/#configuration-files
+var configFile = process.argv[4] || null;
+
+console.log('Executing ' + buildEngine + ' for folder: "' + folderName + '"');
+
+var cmd = buildEngine + ' -c';
+
+if (configFile) {
+  cmd += ' ' + configFile;
+}
+
+exec(cmd, {
+  cwd: folderName
+}, function(error, stdout, stderr) {
+  if (error) {
+    console.log('Error:');
+    console.log(error);
+    console.log(stderr);
+    return;
+  }
+
+  console.log(stdout);
+});
